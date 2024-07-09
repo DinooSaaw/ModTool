b37b1454634be0a1f43c1d47adbe76d601ec633f
DinooSaaw <57213778+DinooSaaw@users.noreply.github.com>
2024-07-09 23:40:45 +1000

    pulling user data

--- preload.js
+++ preload.js
@@ -2,5 +2,11 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
   searchName: (name) => ipcRenderer.send('search-name', name),
-  onDisplayName: (callback) => ipcRenderer.on('display-name', (event, name) => callback(name))
+  onDisplayName: (callback) => ipcRenderer.on('display-name', (event, name) => callback(name)),
+  ipcRenderer: {
+    invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
+    send: (channel, ...args) => ipcRenderer.send(channel, ...args),
+    on: (channel, listener) => ipcRenderer.on(channel, listener)
+  }
+
 });
